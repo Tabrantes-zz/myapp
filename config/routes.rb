c3c4f6d9421686ceed2b1626758c5b216ecf5c8c
Tiago Abrantes <tiago@donorbox.org>
2021-02-25 09:39:10 -0300

    add log out buttom

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  root 'pages#home'
 
-  get 'pages/home'
+  devise_for :users
+  #get 'pages/home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
