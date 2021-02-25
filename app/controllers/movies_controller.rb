782e69ed17ee6200f6de334547db6537539aaa1f
Tiago Abrantes <tiago@donorbox.org>
2021-02-25 09:59:28 -0300

    “Update user model and movies controller”.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,22 +1,48 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def index
+    @movies = Movie.all
   end
 
   def show
   end
 
   def new
+    @movie = Movie.new
+    puts @movie
   end
 
   def create
+    @user = current_user
+    puts @user
+    @movie = @user.movies.create(params_requre)
+    redirect_to movies_path
+
+    #@movie = @user.movies.new(params_requre)
+    # if movie.save
   end
 
   def edit
   end
 
   def update
+    puts "77777777777777777777"
+    @movie.update(params_requre)
+    redirect_to movies_path
   end
 
   def destroy
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+    puts @movie
+  end
+
+  protected
+
+  def params_requre
+    params.require(:movie).permit(:title, :description)
   end
 end
