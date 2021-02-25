bab0ab1f9896e327c20a8967a96a2f812688deb7
Tiago Abrantes <tiago@donorbox.org>
2021-02-25 09:54:39 -0300

    add the movies controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+  get 'movies/show'
+  get 'movies/new'
+  get 'movies/create'
+  get 'movies/edit'
+  get 'movies/update'
+  get 'movies/destroy'
   root 'pages#home'
 
   devise_for :users
